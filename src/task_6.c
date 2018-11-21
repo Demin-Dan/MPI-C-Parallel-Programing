f163500e11e378f822d162bd0de286abb1f87812
Demin-Dan <doriboty@gmail.com>
2018-11-21 12:17:55 +0300

    Update task_6

--- src/task_6.c
+++ src/task_6.c
@@ -21,49 +21,50 @@ int main(int argc, char** argv) {
 		return (EXIT_SUCCESS);
 	}
 	
-	int *lens, *inds, gmx;
+	int *buf, *lbuf, *lens, *inds, gmx;
 	
 	int rws = atoi(argv[1]);
 	int cls = atoi(argv[2]);
 	int st = atoi(argv[3]);
 	int en = atoi(argv[4]);
 	
-	int (*buf)[cls] = malloc(sizeof(*buf) * rws);
+	buf = (int*) malloc(rws * cls * sizeof(int));
 	lens = (int*) malloc(MPI_COMM_SIZE * sizeof(int));
 	inds = (int*) malloc(MPI_COMM_SIZE * sizeof(int));
 	
 	if (MPI_COMM_RANK == 0) {
 		srand(time(NULL));
-		for (int i = 0; i < rws; i ++) { buffer_fill(buf[i], cls, st, en); buffer_print(buf[i], cls); }
+		buffer_fill(buf, rws * cls, st, en);
+		for (int i = 0; i < rws; i ++) {
+			for (int j = 0; j < cls; j ++) printf("%d ", buf[i * cls + j]); printf("\n");
+		}
 	}
 	
 	MPI_Barrier(MPI_COMM_WORLD);
 	
 	buffer_distribute(rws, lens, inds, MPI_COMM_SIZE);
-	
-	int *slns; slns = (int*) malloc(MPI_COMM_SIZE * sizeof(int));
-	for (int i = 0; i < MPI_COMM_SIZE; i ++) { slns[i] = lens[i] * cls; inds[i] = i > 0 ? buffer_sum(slns, 0, i) : 0; }
+	for (int i = 0; i < MPI_COMM_SIZE; i ++) { lens[i] *= buffer_sum(lens, 0, i) < rws * cls ? cls : 0; inds[i] *= cls; }
 	
 	if (MPI_COMM_RANK == 0) {
-		printf("distributed rows "); buffer_print(lens, MPI_COMM_SIZE);
-		printf("distributed lengths "); buffer_print(slns, MPI_COMM_SIZE);
+		printf("distributed lengths "); buffer_print(lens, MPI_COMM_SIZE);
 		printf("distributed indexes "); buffer_print(inds, MPI_COMM_SIZE);
 	}
 	
-	int (*lbuf)[cls] = malloc(sizeof(*lbuf) * lens[MPI_COMM_RANK]);
+	lbuf = (int*) malloc(lens[MPI_COMM_RANK] * sizeof(int));
 	
-	MPI_Scatterv(buf, slns, inds, MPI_INT, lbuf, slns[MPI_COMM_RANK], MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Scatterv(buf, lens, inds, MPI_INT, lbuf, lens[MPI_COMM_RANK], MPI_INT, 0, MPI_COMM_WORLD);
 	
-	if (lens[MPI_COMM_RANK] > 0) printf("thread(%d):\n", MPI_COMM_RANK);
+	if (lens[MPI_COMM_RANK] / cls > 0) printf("thread(%d):\n", MPI_COMM_RANK);
 	int lmx = INT_MIN, lmn;
-	for (int i = 0; i < lens[MPI_COMM_RANK]; i ++) {
-		lmn = buffer_min(lbuf[i], cls);
+	for (int i = 0; i < lens[MPI_COMM_RANK] / cls; i ++) {
+		lmn = lbuf[i * cls];
+		for (int j = 1; j < cls; j ++) { if (lbuf[i * cls + j] < lmn) lmn = lbuf[i * cls + j]; } printf("\tmin(%d) ", lmn);
+		for (int j = 0; j < cls; j ++) printf("%d ", lbuf[i * cls + j]); printf("\n");
 		if (lmn > lmx) lmx = lmn;
-		printf("\tmin(%d) ", lmn); buffer_print(lbuf[i], cls);
 	}
-	if (lens[MPI_COMM_RANK] > 0) printf("\tmaxmin(%d)\n", lmx);
+	if (lens[MPI_COMM_RANK] / cls > 0) printf("\tmaxmin(%d)\n", lmx);
 	
-	free(buf); free(lbuf); free(lens); free(inds); free(slns);
+	free(buf); free(lbuf); free(lens); free(inds);
 	
 	MPI_Reduce(&lmx, &gmx, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
 	
